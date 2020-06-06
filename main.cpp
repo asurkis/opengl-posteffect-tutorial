417e7b8faf99152f5451186e2ed2ac1c90dad90f
Anton Surkis <antonsurkis@gmail.com>
2020-06-06 13:41:53 +0300

    OpenGL init

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstdio>
 #include <functional>
@@ -11,7 +12,7 @@ public:
 };
 
 void myGlfwErrorCallback(int code, const char *description) {
-  printf("[GLFW] %d: %s\n", code, description);
+  printf("[GLFW][code=%d] %s\n", code, description);
   fflush(stdout);
 }
 
@@ -19,6 +20,14 @@ void myGlfwKeyCallback(GLFWwindow *window, int key, int scancode, int action,
                        int mods) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void GLAPIENTRY myGlDebugCallback(GLenum source, GLenum type, GLuint id,
+                                  GLenum severity, GLsizei length,
+                                  const GLchar *message,
+                                  const void *userParam) {
+  printf("[GL][source=0x%X; type=0x%X; id=0x%X; severity=0x%X] %s\n", source,
+         type, id, severity, message);
 }
 
 int main() {
@@ -31,8 +40,17 @@ int main() {
       glfwCreateWindow(800, 600, "OpenGL Tutorial", nullptr, nullptr);
   glfwSetKeyCallback(window, myGlfwKeyCallback);
 
+  glfwMakeContextCurrent(window);
+  if (glewInit() != GLEW_OK)
+    return __LINE__;
+
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback(myGlDebugCallback, nullptr);
+
+  glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
   }
 
